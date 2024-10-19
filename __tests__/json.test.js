f7f9b0e3bf6c7b0e97e4fc734ee09762dae3ff55
Anton <atx157@gmail.com>
2024-10-19 19:26:44 +0600

    added json formatter

---
+++ __tests__/json.test.js
@@ -0,0 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { compareFiles } from '../compare.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+describe('gendiff JSON format', () => {
+  test('compares two JSON files and outputs in JSON format', () => {
+    const filePath1 = getFixturePath('file1.json');
+    const filePath2 = getFixturePath('file2.json');
+
+    const expectedOutput = JSON.parse(readFile('expected_output_json.json'));
+
+    const result = JSON.parse(compareFiles(filePath1, filePath2, 'json').trim());
+
+    expect(result).toEqual(expectedOutput);
+  });
+});
